0029c3dca96b83829fa70ba6577d794850f6a7be
George Kutsia <Georgekutsia@gmail.com>
2022-08-14 02:25:15 +0200

    cambios menores, intentando texto con hover

---
+++ config/popover.config.js
@@ -0,0 +1,8 @@
+const popover = require("@popperjs/core")
+
+import { createPopper } from '@popperjs/core';
+const popcorn = document.querySelector('#popcorn');
+const tooltip = document.querySelector('#tooltip');
+createPopper(popcorn, tooltip, {
+  placement: 'top',
+});
