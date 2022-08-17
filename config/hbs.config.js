a879aae94267af3f4ae91a2087cc8d8845147033
Andrea Galicia <qndreashinee@gmail.com>
2022-08-17 23:08:29 +0200

    helpers may!

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -4,4 +4,6 @@ hbs.registerPartials(__dirname + "/../views/partials");
 
 hbs.registerHelper('prettyDate', (date) =>  date.toLocaleDateString());
 
+hbs.registerHelper('upperCase', (nickname) => nickname.charAt(0).toUpperCase() + nickname.slice(1)) 
+
 hbs.registerHelper('spacesGone', (spaces) => spaces.split('').filter(e => e.trim().length).join(''))
--- models/newexample.model.js
+++ models/newexample.model.js
@@ -11,11 +11,11 @@ const exampleSchema = new Schema ({
     belongs: {
         type:String,
     },
-    exUser: {
+   /* exUser: {
         type: Schema.Types.ObjectId,
         required: true,
         ref: "User",
-      }
+      } */
 })
 
 
