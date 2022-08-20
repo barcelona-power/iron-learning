25dd2e086d04e13f1a83995768c34cb9363907b8
Andrea Galicia <qndreashinee@gmail.com>
2022-08-20 18:55:11 +0200

    base de datos

--- config/db.config.js
+++ config/db.config.js
@@ -5,5 +5,5 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/iron-learnin
 
 mongoose
   .connect(MONGODB_URI)
-  .then(() => console.info("Conectado a la base de datos"))
+  .then(() => console.info("Conectado todo chachi pirulis"))
   .catch((error) => console.error("error al conectar a la base de datos", error));
