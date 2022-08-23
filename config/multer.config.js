64f3d9be84ee043b63a59e8927424a96de4f1c5c
George Kutsia <Georgekutsia@gmail.com>
2022-08-23 17:27:04 +0200

    ya a -1

--- config/multer.config.js
+++ config/multer.config.js
@@ -3,10 +3,15 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require("multer")
  
+const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME 
+const CLOUDINARY_KEY = process.env.CLOUDINARY_KEY 
+const CLOUDINARY_SECRET = process.env.CLOUDINARY_SECRET 
+
+
 cloudinary.config({
-  cloud_name:"du3v1mwzj",
-  api_key:"459475185293298",
-  api_secret:"vNNs7t6xI9rA9xf2LYYkpKxE8jY"
+  cloud_name:CLOUDINARY_NAME,
+  api_key:CLOUDINARY_KEY,
+  api_secret:CLOUDINARY_SECRET
 });
  
 const storage = new CloudinaryStorage({
